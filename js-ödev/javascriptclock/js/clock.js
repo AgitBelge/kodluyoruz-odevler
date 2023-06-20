a9f3abb36877dc262442021da950e2b91ce336c1
Agit belge <agitbelge271@gmail.com>
2023-06-20 12:44:08 +0300

    js ilk ödev

---
+++ js-ödev/javascriptclock/js/clock.js
@@ -0,0 +1,25 @@
+let userName = prompt("isminiz" ,"isminizi girin bu alana");
+let myname = document.getElementById("myName");
+
+myname.innerHTML = `${userName}`;
+
+
+
+
+function time(){
+    let nowTime = new Date();
+    let nowDaye = nowTime.getDay();
+    let nowHours = nowTime.getHours();
+    let nowMinute = nowTime.getMinutes();
+    let nowSecond = nowTime.getSeconds();
+    let days = ["pazartesi","sali","çarşamba","perşembe","cuma","cumartesi","pazar"];
+    let nowdayes = days[nowDaye];
+    console.log(nowdayes);
+
+    let myClock = document.getElementById("myClock");
+    myClock.innerHTML = `${nowHours} : ${nowMinute} : ${nowSecond} ${nowdayes}`;
+
+}
+setInterval(time, 1000);
+
+
